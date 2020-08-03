1fcafce420e70ffdfb3bdd863ca64147584560da
Sheetal R <sheetalraghupathi14@gmail.com>
2020-08-03 14:51:27 +0530

    post order summary to firebase database

--- src/containers/FoodBuilder/FoodBuilder.js
+++ src/containers/FoodBuilder/FoodBuilder.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
+
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Aux from '../../hoc/Auxillary/Auxillary';
+
 
 const ADDONS_PRICES = {
     salad: 20,
@@ -80,6 +83,25 @@ class FoodBuilder extends Component{
         this.setState({orderClicked: false})
     }
 
+    checkoutHandler = () => {
+        const order = {
+            ingredients: this.state.burgerAddOns,
+            totalPrice: this.state.totalPrice, //Total price should actually be calculated in server. The customer can modify this data otherwise
+            customer: {
+                name: 'Sara',
+                address: {
+                    street: 'Lincoln road',
+                    pinCode: '5699',
+                    city: 'bangalore'
+                },
+                email: 'example.com'
+            } 
+        };
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
+    }
+
     //This is not an arrow function. setState is undefined
     // purchaseHandler(){
     //     console.log(this);
@@ -96,7 +118,7 @@ class FoodBuilder extends Component{
                         ingredients={this.state.burgerAddOns}
                         totalPrice={this.state.totalPrice}
                         closeModal={this.closeModal}
-                        checkout={this.checkout}/>
+                        checkout={this.checkoutHandler}/>
                 </Modal>
                 <Burger addOns = {this.state.burgerAddOns}></Burger>
 
