730dc91fcb1650ae7d88991163a4e7ed4a5b91a1
Sheetal R <sheetalraghupathi14@gmail.com>
2020-08-18 16:57:11 +0530

    Lazy load Orders component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { connect } from 'react-redux';
 import Layout from '../src/components/Layout/Layout';
 import FoodBuilder from '../src/containers/FoodBuilder/FoodBuilder';
 import Checkout from '../src/containers/Checkout/Checkout';
-import Orders from '../src/components/Order/Orders/Orders';
+// import Orders from '../src/components/Order/Orders/Orders';
 import Auth from '../src/containers/Auth/Auth';
 import Logout from '../src/containers/Auth/Logout';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import * as actions from './store/actions/index';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
+
+const AsyncOrders = asyncComponent(() => {
+  return import('../src/components/Order/Orders/Orders');
+})
 
 class App extends Component{
 
@@ -29,7 +34,7 @@ class App extends Component{
       routes = (
         <Switch>
           <Route path="/checkout" component={Checkout}/>
-          <Route path="/orders" component={Orders}/>
+          <Route path="/orders" component={AsyncOrders}/>
           <Route path="/logout" component={Logout}/>
           <Route path="/" component={FoodBuilder}/>
           <Redirect to="/"/>
